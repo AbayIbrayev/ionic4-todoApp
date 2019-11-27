05b6ac962f7ba791abdf078ca12987d538a50e87
Abay <abayibrayev7@gmail.com>
2019-11-27 16:26:41 +0000

    initial commit + simple to-do list added
    to-do list added

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  taskList = [];
+  taskName: any;
+
+  constructor(public navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  addTask() {
+    if (this.taskName.length > 0) {
+      let task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = "";
+    }
+  }
+
+}
