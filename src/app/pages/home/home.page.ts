9808783061bf298bea30e1ed2e31769b4d263bee
Abay <abayibrayev7@gmail.com>
2019-11-28 09:57:35 +0000

    Update and Delete task functionality added + Fade In animation added
    - Update and Delete your tasks functinality added
    - Fade In animation added to the AddTask method

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  animations: [
+    trigger('fadein', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('900ms ease-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class HomePage implements OnInit {
   taskList = [];
   taskName: any;
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
@@ -21,6 +37,28 @@ export class HomePage implements OnInit {
       this.taskList.push(task);
       this.taskName = "";
     }
+  }
+
+  deleteTask(index) {
+    setTimeout(() => {
+      this.taskList.splice(index, 1);
+    }, 200)
+  }
+
+  async updateTask(index) {
+    const alert = await this.alertCtrl.create({
+      header: 'Update Task?',
+      message: 'Type in your new task to update.',
+      inputs: [{ name: 'editTask', placeholder: 'Task' }],
+      buttons: [{ text: 'Cancel', role: 'cancel' },
+      {
+        text: 'Update', handler: data => {
+          this.taskList[index] = data.editTask;
+        }
+      }
+      ]
+    });
+    await alert.present();
   }
 
 }
